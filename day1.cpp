5268a1e0e380a8fd5189d9b78a3d91fc969d88a7
TheAssembler1 <lewisnoah592@gmail.com>
2024-10-11 15:49:37 -0400

    first problem

---
+++ day1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main() {
+	std::ifstream input{"/home/ta1/src/aoc_2023/in.txt", std::ifstream::in};
+	std::vector<std::string> lines{};
+	for (std::string s; std::getline(input, s, '\n'); lines.push_back(s));
+	int total = 0;
+
+	for(const std::string& s: lines) {
+		int left = 0;
+		int right = s.size() - 1;
+
+		while(left <= right) {
+			char l = s[left];
+			char r = s[right];
+
+			if(left == right) {
+				total += std::stoi(std::string{l} + std::string{r});
+				break;
+			}
+			if(l >= '0' && l <= '9' && r >= '0' && r <= '9') {
+				total += std::stoi(std::string{l} + std::string{r});
+				break;
+			}
+
+			if(l < '0' || l > '9') left++;
+			if(r < '0' || r > '9') right--;
+		}
+	}
+
+	std::cout << total;
+
+	return total;
+}
